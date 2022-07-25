fd03e15f9d3677321abca2f23ba6c87f2a370d82
solanem <anitasz@live.ca>
2022-07-25 15:52:22 +0200

    add dto and messageProducer

---
+++ src/main/java/com/kbe/apigateway/messageproducer/producer/CurrencyProducer.java
@@ -0,0 +1,33 @@
+package com.kbe.apigateway.messageproducer.producer;
+
+import messageproducer.config.Constant;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/rabbit")
+public class CurrencyProducer {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @GetMapping(value = "/currency", produces = "application/json")
+    public ResponseEntity<Double> getCurrency(@RequestBody String currencies) {
+        Double receivedCurrency = template.convertSendAndReceiveAsType(
+                Constant.CURRENCY_EXCHANGE,
+                Constant.GET_KEY,
+                currencies,
+                new ParameterizedTypeReference<>() {}
+        );
+        if(receivedCurrency == null) return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(receivedCurrency, HttpStatus.OK);
+    }
+}
